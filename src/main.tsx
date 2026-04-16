cb62afced215d89434d019cd65634c77538b94ea
Ilia Kriachkov <ldrner@gmail.com>
2026-04-16 23:36:19 +0300

    refactor: replace ReactDOM.createRoot with ViteReactSSG

--- src/main.tsx
+++ src/main.tsx
@@ -1,26 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter as Router } from 'react-router-dom'
-import { HeroUIProvider, ToastProvider } from '@heroui/react'
-import { HelmetProvider } from 'react-helmet-async'
+import { ViteReactSSG } from 'vite-react-ssg'
 import App from './App'
+import { routes } from './routes'
 import './index.css'
 import './i18n'
 
-window.addEventListener('unhandledrejection', event => {
-  console.error('Unhandled promise rejection:', event.reason)
-  event.preventDefault()
-})
+if (typeof window !== 'undefined') {
+  window.addEventListener('unhandledrejection', event => {
+    console.error('Unhandled promise rejection:', event.reason)
+    event.preventDefault()
+  })
+}
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <HelmetProvider>
-      <Router>
-        <HeroUIProvider>
-          <ToastProvider />
-          <App />
-        </HeroUIProvider>
-      </Router>
-    </HelmetProvider>
-  </React.StrictMode>
-)
+export const createRoot = ViteReactSSG(App, { routes })
